64cb8e607e13376b4bca42f5ec3283572893ca0d
Alfredo Lozano <alfredo.lozano@newral.com>
2023-06-22 19:58:54 -0600

    Feat: first commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+export default function Home() {
+  return (
+    <>
+      <h2>hola</h2>
+      <Link href="/nosotros">Nosotros</Link>
+    </>
+  )
+}
---
+++ pages/nosotros.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+function Nosotros() {
+  return (
+    <div>
+      Nosotros
+      <a href="/">Ir a inicio</a>
+    </div>
+  );
+}
+
+export default Nosotros;
